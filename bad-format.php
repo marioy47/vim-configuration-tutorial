7a615d95ce37da391d358d57cfdd73fa5d49ce98
Mario Yepes <marioy47@gmail.com>
2020-05-27 11:27:10 -0500

    Finishing the config.vim file

---
+++ bad-format.php
@@ -0,0 +1,8 @@
+<?php
+/* This is a badly formatted php file to test the linters */
+class HelloWorld {
+
+    public function __construct() {
+        echo "HelloWorld instansce created"; }    
+ 
+}
